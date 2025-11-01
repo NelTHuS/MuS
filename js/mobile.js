195ac2123e519a04cfdf5273f134e203ff736db7
NelTHuS <67994895+NelTHuS@users.noreply.github.com>
2025-11-01 16:51:10 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "NeO'S MuSiC";
     }
 
     function updateMobileOverlayScrim() {
